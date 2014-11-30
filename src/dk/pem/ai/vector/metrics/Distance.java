2e2d1d47f86a467472fc062b23dcb89a9752d0ee
MikkelsenPer <mikkelsen.p@gmail.com>
2014-11-30 18:43:40 +0100

    Added code doc

--- src/dk/pem/ai/vector/metrics/Distance.java
+++ src/dk/pem/ai/vector/metrics/Distance.java
@@ -1,9 +1,15 @@
 package dk.pem.ai.vector.metrics;
 
-
+/**
+ * Class for calculating distances between vectors.
+ * @author Per Mikkelsen
+ *
+ */
 public class Distance {
 
-	
+	/**
+	 * Returns the Euclidean distance between to vectors.
+	 */
 	public double getEuclideanDistance(double[] vectorA, double[] vectorB) {
 		double sum = 0;
 		for (int i = 0; i < vectorA.length; i++) {
